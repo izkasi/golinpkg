c2cb6676f87a4e8ff2aaea48e351c98faa493043
Alexander Frimmel <alex@sneer.at>
2020-05-30 17:55:30 +0200

    INITIAL PACKAGE

---
+++ package_test.go
@@ -0,0 +1,30 @@
+package dpkg
+
+import (
+	"bufio"
+	"os"
+	"testing"
+)
+
+func TestParseStatus(t *testing.T) {
+
+	fileHandle, _ := os.Open("tests/status")
+	defer fileHandle.Close()
+
+	fileScanner := bufio.NewScanner(fileHandle)
+
+	pkgs := ParseStatus(fileScanner)
+
+	if len(pkgs) != 23 {
+		t.Errorf("Number of parsed packages was incorrect, got: %d, want: %d.", len(pkgs), 23)
+	}
+
+	if pkgs[10].Package != "bind9-host" {
+		t.Errorf("Parsed package was incorrect, got: %s, want: %s.", pkgs[10].Package, "bind9-host")
+	}
+
+	if len(pkgs[9].Conffiles) != 3 {
+		t.Errorf("Parsed package Conffiles was incorrect, got: %d, want: %d.", len(pkgs[9].Conffiles), 3)
+	}
+
+}
